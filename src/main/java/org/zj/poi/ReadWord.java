13df9093105a9c14762151610afd61c22977b250
blague <zjblague@gmail.com>
2013-04-05 16:53:07 +0800

    Readword,从飞友网抓取机场数据 其他,增加h2db支持,

--- src/main/java/org/zj/poi/ReadWord.java
+++ src/main/java/org/zj/poi/ReadWord.java
@@ -1,23 +1,33 @@
 package org.zj.poi;
 
-import org.apache.poi.hwpf.extractor.WordExtractor;
+import  org.jsoup.nodes.Document;
+import  org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
-import java.io.*;
+import java.io.IOException;
 
 public class ReadWord {
 
     public static final  String path = "C:\\Users\\blague\\Downloads\\机场代码.doc";
 
     public static void main(String[] args) {
+        String url = "http://www.feeyo.com/airport_code.asp?page=";
         try {
-            FileInputStream in = new FileInputStream(new File(path));
-            WordExtractor extrator = new WordExtractor(in);
-            String[] strs = extrator.getParagraphText();
+            for (int k = 1; k <=60 ; k++) {
+                Document doc = Jsoup.connect(url+k).get();
+                Elements element = doc.select(".tab_hand tr");
+                for (int i = 1,length = element.size()-1; i < length ; i++) {
+                    Elements tds = element.get(i).select("td");
+                    System.out.print("insert into AIRPORTCODE  VALUES(");
+                    for (int j = 0,jlenth = tds.size()-1; j < jlenth ; j++) {
+                        System.out.print("'"+tds.get(j).text() + "'");
+                    }
+                    System.out.println(");");
+                }
+            }
 
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
 
