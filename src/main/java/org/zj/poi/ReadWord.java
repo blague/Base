b69d5fb514fde43763f2acff61090d4ba33c72e8
blague <zjblague@gmail.com>
2013-04-06 22:28:04 +0800

    add 地区实体类 edite 见TODO

--- src/main/java/org/zj/poi/ReadWord.java
+++ src/main/java/org/zj/poi/ReadWord.java
@@ -10,10 +10,14 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCallback;
 import org.springframework.util.StringUtils;
+import org.zj.pojo.Area;
 
 import java.io.IOException;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class ReadWord {
 
@@ -26,28 +30,45 @@ public class ReadWord {
      * 插入地区数据
      */
     public static final String INSERT_AREA = "insert into area values (null,?,?)";
+    /**
+     * 查询对应的三字码的国家,洲
+     */
+    public static final String QUERY_COUNTY_AREA  = "select id,name,parent from Area where  name  = ?";
+
+    /**
+     * 根据三字码修改对应城市所在洲,国家
+     */
+    public static final String UPDATE_AIRPORTCODE_COUNTY = "UPDATE AIRPORTCODE SET  COUNTY = ? ,CONTINENT = ? WHERE THREECODE  = ?";
+
+
+
 
     public static void main(String[] args) {
         ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
         JdbcTemplate jdbcTemplate = appContext.getBean("jdbcTemplate", JdbcTemplate.class);
 
-        String url = "http://www.51ticket.net/ziliao/aozhou1.htm";
-        try {
-            Document doc = Jsoup.connect(url).get();
-            Elements element = doc.select("#table1 tr");
-            for (int i = 1,length = element.size(); i < length; i++) {
-                final String name = element.get(i).child(3).text();
-                if(!Strings.isNullOrEmpty(name) && jdbcTemplate.queryForInt(QUERY_COUNT_AREA,name) < 1){
-                    jdbcTemplate.execute(INSERT_AREA,new PreparedStatementCallback<Boolean>() {
-                        @Override
-                        public Boolean doInPreparedStatement(PreparedStatement ps) throws SQLException, DataAccessException {
-                            ps.setString(1,name);
-                            ps.setInt(2,5);
-                            return ps.execute();
-                        }
-                    });
-                }
 
+        Map<String,String> urlMap = new HashMap<String, String>();
+        urlMap.put("1","http://www.51ticket.net/ziliao/yazhou.htm");
+        urlMap.put("2","http://www.51ticket.net/ziliao/ouzhou.htm");
+        urlMap.put("3","http://www.51ticket.net/ziliao/nammei.htm");
+        urlMap.put("3","http://www.51ticket.net/ziliao/beimei.htm");
+        urlMap.put("4","http://www.51ticket.net/ziliao/feizhou.htm");
+        urlMap.put("5","http://www.51ticket.net/ziliao/aozhou1.htm");
+
+        try {
+            Map<String,Area> areaMap = new HashMap<String, Area>();
+            Iterator<Map.Entry<String,String>> iterator =urlMap.entrySet().iterator();
+            while(iterator.hasNext()){
+                Map.Entry<String, String> enty =iterator.next();
+                Document doc = Jsoup.connect(enty.getValue()).get();
+                Elements element = doc.select("#table1 tr");
+                for (int i = 1,length = element.size(); i < length; i++) {
+                    final String name = element.get(i).child(1).text();
+                    //TODO 将所有国家的机场三字码全部抓取,放置到areMap中.2013年4月6日 22:19:45
+
+
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
---
+++ src/main/java/org/zj/pojo/Area.java
@@ -0,0 +1,55 @@
+package org.zj.pojo;
+
+import java.util.List;
+
+/**
+ * 地区信息
+ */
+public class Area {
+
+    private int id;
+
+    private String name;
+
+    private int parent;
+
+    /**
+     * 所在国家的机场三字码集合
+     */
+    private List<String> citys;
+
+    public Area() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getParent() {
+        return parent;
+    }
+
+    public void setParent(int parent) {
+        this.parent = parent;
+    }
+
+    public List<String> getCitys() {
+        return citys;
+    }
+
+    public void setCitys(List<String> citys) {
+        this.citys = citys;
+    }
+}
